1c5cb05cf51cdb2c87070075b1670f379a8114b8
Oleksandr Vitiuk <96valex@gmail.com>
2024-04-23 09:57:58 +0300

    Lab_2_upd

--- src/Patterns/Structural/Adapter/FlightBooking.java
+++
@@ -1,7 +0,0 @@
-package Patterns.Structural.Adapter;
-
-public class FlightBooking {
-    public void reserveFlight(String destination, String date) {
-        System.out.println("Flight reserved to " + destination + " on " + date);
-    }
-}
--- src/Patterns/Structural/Adapter/HotelBooking.java
+++
@@ -1,7 +0,0 @@
-package Patterns.Structural.Adapter;
-
-public class HotelBooking {
-    public void bookHotel(String hotelName, int nights) {
-        System.out.println("Hotel room booked at " + hotelName + " for " + nights + " nights");
-    }
-}
